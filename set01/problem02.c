775ba1637dc3982a9666aea9e862a4a8e9abf195
Khushi <khushia.bsc23@rvu.edu.in4>
2024-01-29 06:39:11 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,16 @@
 //2. Write a C program to add two numbers.
+#include<stdio.h>
+int main(){
 
+    int a,b,sum;
+    printf("enter first number:");
+    scanf("%d" , &a);
+
+    printf("enter second number:");
+    scanf("%d" , &b);
+
+    sum=a+b;
+
+    printf("The sum of %d + %d is %d" , a,b,sum);
+    return 0;
+}
---
+++ set01/problem03.c
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+int input();
+int add(int a, int b);
+void output(int a, int b, int sum);
+
+int main()
+{ 
+    int a,b,sum;
+    a=input();
+    b=input();
+    sum=add(a,b);
+    output(a,b,sum);
+}
+
+int input(){
+    int n;
+
+    printf("enter first number:");
+    scanf("%d" , &n);
+    return n;
+
+
+}
+int add(int a, int b){
+
+    int sum;
+    sum=a+b;
+    return sum;
+}
+
+void output(int a, int b, int sum){
+
+    printf("the sum of %d + %d is %d" , a , b ,sum);
+}
+
+
+
+   
+
