94f5485035281a808e576848e623aa2afcf3296a
Khushi <khushia.bsc23@rvu.edu.in4>
2024-02-16 14:15:31 +0530

    test

--- set01/problem06.c
+++ set01/problem06.c
@@ -1,37 +1,32 @@
-#include <stdio.h>
-int input(int *a, int *b, int *c);
-void compare(int a, int b, int c, int *largest);
-void output(int a, int b, int c, int largest);
+#include<stdio.h>
+
+int input(int *num){
+    printf("enter a number: ");
+    scanf("%d", num);
+}
+
+void compare(int a, int b, int c, int *lar){
+   if (a>=b && a>=c){
+        *lar=a;
+    }
+    else if(b>=a && b>=c){
+        *lar=b;
+    }
+    else{
+        *lar=c;
+    }
+}
+
+int output(int a, int b, int c, int lar){
+    printf("the largest number between %d %d and %d is %d", a,b,c,lar);
+}
 
 int main(){
-
-     int a,b,c,largest;
+    int a,b,c,largest;
     input(&a);
     input(&b);
     input(&c);
-    largest=compare(a,b,c);
+    compare(a,b,c,&largest);
     output(a,b,c,largest);
     return 0;
 }
- int input (int *a , int*b , int*c){
-
-    
-    int n ;
-    printf("enter a number:");
-    scanf("%d" ,n);
-
- }
- 
- void compare(int a,int b,int c , int*largest){
-    if(a>=b && a>=c);
-    return a;
-    else if(b>=a && b>=c)
-    return b;
-    else
-    return c;
- }
-
-
-
-
-
